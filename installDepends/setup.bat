f50d520291866590c3c2987063286dd3b92005e0
Sky <rufatkiu@mail.ru>
2025-05-17 01:19:53 +0800

    Add files via upload

---
+++ installDepends/setup.bat
@@ -0,0 +1,1 @@
+runas /user:Administrator /profile   "pip3 install deepface --no-cache-dir --force-reinstall -–upgrade  -i https://pypi.tuna.tsinghua.edu.cn/simple&pip3 install Pillow --no-cache-dir --force-reinstall –-upgrade  -i https://pypi.tuna.tsinghua.edu.cn/simple&pip3  install tf-keras  --no-cache-dir --force-reinstall –-upgrade  -i https://pypi.tuna.tsinghua.edu.cn/simple"
